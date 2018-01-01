156ba2387a5637a16cbb66f0c2819497d6e06da0
lauriescheepers <laurscheepers@gmail.com>
2018-01-01 20:54:49 +0200

    Trying to get DataBindingAdapters to work again, no idea why it's not working

--- app/src/main/java/laurcode/com/retire/databinding/DataBindingAdapters.java
+++ app/src/main/java/laurcode/com/retire/databinding/DataBindingAdapters.java
@@ -1,38 +1,24 @@
 package laurcode.com.retire.databinding;
 
-import android.animation.ValueAnimator;
-import android.annotation.SuppressLint;
 import android.databinding.BindingAdapter;
-import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
-import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.facebook.rebound.SimpleSpringListener;
 import com.facebook.rebound.Spring;
 import com.facebook.rebound.SpringConfig;
 import com.facebook.rebound.SpringSystem;
 
-import java.util.Arrays;
-
-import laurcode.com.retire.R;
-import laurcode.com.retire.util.ViewUtil;
 import timber.log.Timber;
 
 /**
- * Collection of custom data binding resource attributes
- * <p>
- * Created by lauriescheepers on 2017/11/16.
+ * Insert description of class here
+ * <br><br>
+ * Created by lauriescheepers on 2018/01/01.
  */
 
-@SuppressWarnings("WeakerAccess")
 public class DataBindingAdapters {
 
     // Adjusting the TENSION constant will change the speed of the animation; the higher the tension the faster the speed.
@@ -41,45 +27,19 @@ public class DataBindingAdapters {
     // Adjusting the DAMPER constant will change the bounce at the end of the animation, the lower the damper the more bounce.
     private static final double SPRING_DAMPER = 9;     // also called friction
 
-    private static boolean springTranslateMovedUp;
-    private static boolean springTranslateMovedDown;
-    private static float springTranslateOrigY;
-
-    @BindingAdapter("imageUrl")
-    public static void loadImageUrl(ImageView view, String url) {
-        // Load into image view with Glide
-//        GlideApp
-//                .with(view)
-//                .load(url)
-//                .placeholder(new ColorDrawable(ContextCompat.getColor(view.getContext(), R.color.placeholder_bg_color)))
-//                .error(R.drawable.ic_error_404)
-//                .transition(DrawableTransitionOptions.withCrossFade())
-//                .into(view);
-    }
-
-    @BindingAdapter("imageRes")
-    public static void loadImageRes(ImageView view, @DrawableRes int resId) {
-        if (ViewUtil.isViewVisible(view)) {
-            ViewUtil.fadeViewOut(view, () -> {
-                // Load the image as a resource
-                view.setImageResource(resId);
-                ViewUtil.fadeViewIn(view);
-            });
-        } else {
-            view.setImageResource(resId);
-            ViewUtil.fadeViewIn(view);
-        }
-    }
-
     /**
      * Adds a bounce animation to the given view when touched.
      */
-    @BindingAdapter("bounceOnTouch")
+    @BindingAdapter("android:bounceOnTouch")
     public static void bounceOnTouch(@NonNull View view, boolean bounce) {
+
+        Timber.d("BOUNCING ON TOUCH!");
 
         if (!bounce) {
             return;
         }
+
+        Timber.d("SETTING UP SPRING!");
 
         Spring spring = SpringSystem.create().createSpring();
 
@@ -124,122 +84,5 @@ public class DataBindingAdapters {
             }
             return true;
         });
-    }
-
-    /**
-     * Adds a translate animation (up) to the given view when clicked.
-     */
-    @BindingAdapter("moveUpOnClick")
-    public static void moveUpOnClick(@NonNull View view, int value) {
-
-        springTranslateMovedUp = false;
-        springTranslateOrigY = 0;
-
-        if (value == 0) {
-            return;
-        }
-
-        Spring spring = SpringSystem.create().createSpring();
-
-        SpringConfig config = new SpringConfig(SPRING_TENSION, SPRING_DAMPER);
-
-        spring.setSpringConfig(config);
-
-        spring.addListener(new SimpleSpringListener() {
-
-            @Override
-            public void onSpringUpdate(Spring spring) {
-                float value = (float) spring.getCurrentValue();
-
-                if (ViewCompat.isAttachedToWindow(view)) {
-                    view.setY(value);
-                }
-            }
-        });
-
-        view.setOnClickListener(v -> {
-            if (springTranslateMovedUp) {
-                spring.setEndValue(springTranslateOrigY);
-            } else {
-                springTranslateOrigY = view.getY();
-
-                spring.setEndValue(springTranslateOrigY - value);
-            }
-
-            springTranslateMovedUp = !springTranslateMovedUp;
-        });
-    }
-
-    /**
-     * Adds a translate animation (up) to the given view when clicked.
-     */
-    @BindingAdapter("moveDownOnClick")
-    public static void moveDownOnClick(@NonNull View view, int value) {
-
-        springTranslateMovedDown = false;
-        springTranslateOrigY = 0;
-
-        if (value == 0) {
-            return;
-        }
-
-        Spring spring = SpringSystem.create().createSpring();
-
-        SpringConfig config = new SpringConfig(SPRING_TENSION, SPRING_DAMPER);
-
-        spring.setSpringConfig(config);
-
-        spring.addListener(new SimpleSpringListener() {
-
-            @Override
-            public void onSpringUpdate(Spring spring) {
-                float value = (float) spring.getCurrentValue();
-
-                if (ViewCompat.isAttachedToWindow(view)) {
-                    view.setY(value);
-                }
-            }
-        });
-
-        view.setOnClickListener(v -> {
-            if (springTranslateMovedDown) {
-                spring.setEndValue(springTranslateOrigY);
-            } else {
-                springTranslateOrigY = view.getY();
-
-                spring.setEndValue(springTranslateOrigY + value);
-            }
-
-            springTranslateMovedDown = !springTranslateMovedDown;
-        });
-    }
-
-    @SuppressLint("SetTextI18n")
-    @BindingAdapter("drawText")
-    public static void drawText(@NonNull TextView textView, boolean draw) {
-
-        if (!draw) {
-            return;
-        }
-
-        int length = textView.getText().length();
-
-        char[] chars = new char[length];
-
-        // Fill the char array
-        TextUtils.getChars(textView.getText(), 0, length, chars, 0);
-
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(chars[0], chars[length - 1]);
-        valueAnimator.setDuration(1000);
-
-        valueAnimator.addUpdateListener(animation -> {
-            String text = textView.getText().toString().concat(Arrays.toString(Character.toChars((Integer) valueAnimator.getAnimatedValue())));
-
-            Timber.d("Text is now: " + text);
-
-            textView.setText(text);
-        });
-
-        valueAnimator.start();
     }
 }
--- app/src/main/java/laurcode/com/retire/startup/SplashActivity.java
+++ app/src/main/java/laurcode/com/retire/startup/SplashActivity.java
@@ -1,7 +1,20 @@
 package laurcode.com.retire.startup;
 
 import android.app.Activity;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import laurcode.com.retire.R;
+import laurcode.com.retire.base.BaseActivity;
+import laurcode.com.retire.base.BaseFragment;
+import laurcode.com.retire.base.BasePresenter;
+import laurcode.com.retire.base.BaseViewModel;
+import timber.log.Timber;
 
 /**
  * Insert description of class here
@@ -9,5 +22,70 @@ import android.support.v7.app.AppCompatActivity;
  * Created by lauriescheepers on 2018/01/01.
  */
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
+
+    @BindView(R.id.continue_button)
+    Button continueButton;
+
+    @Override
+    public void setBundle(Bundle bundle) {
+        // Nothing needed
+    }
+
+    @Override
+    public void onChildFragmentAttached(BaseFragment childFragment) {
+        // Nothing needed
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+        continueButton.setOnClickListener(view -> Timber.d("Continue clicked!"));
+    }
+
+    @Override
+    protected boolean isMvpvm() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    protected Class getPresenterClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected Class getViewModelClass() {
+        return null;
+    }
+
+    @Override
+    protected void bindToActivity(ViewDataBinding binding, BaseViewModel viewModel, BasePresenter presenter) {
+        // Nothing needed
+    }
+
+    @Override
+    public void onDataLoaded() {
+        // Nothing needed
+    }
+
+    @Override
+    public void onFragmentAttached(BaseFragment fragment) {
+        // Nothing needed
+    }
+
+    @Override
+    public void onFragmentDetached(BaseFragment fragment) {
+        // Nothing needed
+    }
+
+    @Override
+    public void onFragmentResumed(BaseFragment fragment) {
+        // Nothing needed
+    }
 }
