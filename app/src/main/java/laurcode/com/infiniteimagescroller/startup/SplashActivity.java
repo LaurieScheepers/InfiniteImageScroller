b21e3c2fa4d5423507c535be011c5b3e2f979407
lauriescheepers <laurscheepers@gmail.com>
2018-01-01 22:53:07 +0200

    Add the launcher icons

--- app/src/main/java/laurcode/com/infiniteimagescroller/startup/SplashActivity.java
+++ app/src/main/java/laurcode/com/infiniteimagescroller/startup/SplashActivity.java
@@ -55,46 +55,6 @@ public class SplashActivity extends AppCompatActivity {
 
     @BindView(R.id.root_container)
     RelativeLayout rootContainer;
-//    @BindView(R.id.ape_to_digital_man_image)
-//    ImageView evolutionImage;
-//    @BindView(R.id.company_name)
-//    TyperTextView companyNameTextView;
-//    @BindView(R.id.app_name)
-//    TyperTextView appNameTextView;
-//    @BindView(R.id.black_friday_section)
-//    EasyFlipView blackFridayFlipView;
-//    @BindView(R.id.view_black_friday_section_front)
-//    LinearLayout blackFridaySectionFront;
-//    @BindView(R.id.view_black_friday_section_back)
-//    LinearLayout blackFridaySectionBack;
-//    @BindView(R.id.black_friday_hint)
-//    TextView blackFridayHint;
-//    @BindView(R.id.black_friday_hint_hint)
-//    TextView blackFridayHintHint;
-//    @BindView(R.id.black_friday_hint_image)
-//    ImageView blackFridayHintImage;
-//    @BindView(R.id.black_friday_deals_heading)
-//    TextView blackFridayDealsHeading;
-//    @BindView(R.id.black_friday_deals_link)
-//    TextView blackFridayDealsLink;
-//    @BindView(R.id.black_friday_link_hint)
-//    TextView blackFridayLinkHint;
-//    @BindView(R.id.pacman_loading_indicator)
-//    AVLoadingIndicatorView pacManLoadingIndicator;
-//    @BindView(R.id.syncing_data_text_view)
-//    TextView syncingDataTextView;
-//    @BindView(R.id.indicating_arrow)
-//    ImageView continueIndicatingArrow;
-//    @BindView(R.id.click_me_to_continue)
-//    TextView continueTextView;
-
-    public static final String BLACK_FRIDAY_URL = "https://superbalist.com/showdown";
-
-    // A flag indicating that the main ape-to-man evolution image is finished
-    private boolean mainImageAnimationDone;
-
-    // A flag indicating that the service has finished with the API call
-    private boolean serviceFinishedWork;
 
     // A flag indicating that the user has seen this splash before
     private boolean userHasSeenSplashBefore;
@@ -104,6 +64,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         userHasSeenSplashBefore = SharedPreferencesUtil.hasUserSeenSplash(this);
+
+        if (userHasSeenSplashBefore) {
+            goToMainActivity();
+            return;
+        }
 
         // Get a handle on the binding
         ActivitySplashBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
@@ -119,204 +84,11 @@ public class SplashActivity extends AppCompatActivity {
 
         // Bind Butterknife for easy access to the views in this activity
         ButterKnife.bind(this);
-
-        splashViewModel.setBlackFridayUrl(BLACK_FRIDAY_URL);
-
-        // Start le nice animations
-        startAnimations();
-
-//        // Click listeners for black friday section
-//        blackFridaySectionFront.setOnClickListener(v -> {
-//            blackFridayFlipView.flipTheView();  // Flippit
-//
-//            blackFridayDealsHeading.setVisibility(View.VISIBLE);
-//            blackFridayDealsLink.setVisibility(View.VISIBLE);
-//            blackFridayLinkHint.setVisibility(View.VISIBLE);
-//
-//            blackFridayDealsLink.setOnClickListener(v1 -> {
-//                // Launch the browser
-//                Intent i = new Intent(Intent.ACTION_VIEW);
-//                i.setData(Uri.parse(BLACK_FRIDAY_URL));
-//                startActivity(i);
-//            });
-//        });
-//
-//        blackFridaySectionBack.setOnClickListener(v -> {
-//            blackFridayFlipView.flipTheView();  // Flippit
-//
-//            blackFridayFlipView.postDelayed(() -> {
-//                blackFridayDealsHeading.setVisibility(View.INVISIBLE);
-//                blackFridayDealsLink.setVisibility(View.INVISIBLE);
-//                blackFridayLinkHint.setVisibility(View.INVISIBLE);
-//            }, 400);    // Add a bit of delay here
-//        });
-//
-//        // These two text views must be empty by default
-//        companyNameTextView.setText("");
-//        appNameTextView.setText("");
-//
-//        evolutionImage.setOnClickListener(v -> {
-//            if (serviceFinishedWork && mainImageAnimationDone) {
-//                SplashActivity.this.goToMainActivity();
-//            }
-//        });
-    }
-
-    private void startAnimations() {
-        List<Integer> drawables = new ArrayList<>();
-
-        showProgress();
-
-//        // Sanity check to ensure the image is not visible
-//        if (ViewUtil.isViewVisible(evolutionImage)) {
-//            evolutionImage.setVisibility(View.GONE);
-//        }
-
-        // The drawables we want to show
-        // NOTE: If we have already been through the whole splash screen, just show the last image
-        if (!userHasSeenSplashBefore) {
-            drawables.add(R.drawable.ape_branch_final);
-            drawables.add(R.drawable.ape_stick_final);
-            drawables.add(R.drawable.man_tool_final);
-        }
-
-        drawables.add(R.drawable.man_digital_final);
-
-//        // Start the custom image drawable fader
-//        new ImageDrawableFader()
-//                .setDrawables(drawables)
-//                .start(evolutionImage, new ImageDrawableFaderListener() {
-//
-//            @Override
-//            public void onComplete() {
-//                ViewUtil.fadeViewIn(companyNameTextView,
-//                    // Now fade in all the things in a logical order (in a top to bottom way)
-//                    () -> {
-//                        companyNameTextView.setAnimationListener(hTextView -> ViewUtil.fadeViewIn(appNameTextView,
-//                            () -> {
-//                                appNameTextView.setAnimationListener(
-//                                    hTextView1 -> ViewUtil.fadeViewIn(blackFridayHint,
-//                                        () -> ViewUtil.fadeViewIn(blackFridayHintHint,
-//                                            () -> ViewUtil.fadeViewIn(blackFridayHintImage,
-//                                                () -> {
-//                                                    mainImageAnimationDone = true;
-//
-//                                                    // If we get here and the service is done, show the arrow indicating where to click
-//                                                    if (serviceFinishedWork) {
-//                                                        showIndicatingArrowHint();
-//                                                    }
-//                                                }))));
-//
-//                                // Start type-writer for app name
-//                                appNameTextView.animateText(getString(R.string.app_name));
-//                            }));
-//
-//                        // Start type-writer for company name (LaurCode)
-//                        companyNameTextView.animateText(getString(R.string.company_name));
-//                    });
-//            }
-//
-//            @Override
-//            public void onError(Exception e) {
-//                // Ignore the error and go ahead and show the text underneath the image. Just log it at least.
-//                CrashUtil.logCrash("There was an error with the image drawable fader, but life must go on. Ignoring.", e);
-//                onComplete();
-//            }
-//        });
     }
 
     private void goToMainActivity() {
         SharedPreferencesUtil.setUserHasSeenSplash(this, true);
 
-//        rootContainer.postDelayed(() -> {
-//            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-//
-//            // Finish SplashActivity so that it doesn't appear on the back stack
-//            finish();
-//        }, 750);
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onEvent(PhotosRetrievedSuccessEvent event) {
-        Timber.d("We have a successful response for photos retrieved!");
-
-        serviceFinishedWork = true;
-
-        // Success! Hide the progress indicator
-        hideProgress();
-
-        // Show the user where to press to continue
-        showIndicatingArrowHint();
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onEvent(PhotosRetrievedFailedEvent event) {
-
-        serviceFinishedWork = false;
-
-        // It failed - hide the progress indicator
-        hideProgress();
-
-        // And show a snackity-snack snackbar
-        showErrorSnackbar();
-    }
-
-    private void showProgress() {
-//        ViewUtil.fadeViewIn(pacManLoadingIndicator, () -> {
-//            pacManLoadingIndicator.show();
-//            ViewUtil.fadeViewIn(syncingDataTextView);
-//        });
-    }
-
-    private void hideProgress() {
-//        ViewUtil.fadeViewOut(pacManLoadingIndicator, () -> ViewUtil.fadeViewOut(syncingDataTextView));
-    }
-
-    private void showIndicatingArrowHint() {
-        // The arrow should only be shown once the main image animation is done (I want people to look at it, see what it means)
-//        if (mainImageAnimationDone) {
-//            ViewUtil.fadeViewIn(continueIndicatingArrow, () -> ViewUtil.fadeViewIn(continueTextView));
-//        }
-    }
-
-    private void hideIndicatingArrowHint() {
-//        ViewUtil.fadeViewOut(continueTextView, () -> ViewUtil.fadeViewOut(continueIndicatingArrow));
-    }
-
-    private void showErrorSnackbar() {
-//        SnackbarWrapper.show(rootContainer, R.string.error_loading_photos, Snackbar.LENGTH_LONG, R.string.try_again, v -> {
-//
-//            // If try again is pressed show progress again
-//            showProgress();
-//
-//            hideIndicatingArrowHint();
-//        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        // Register event bus if not registered already
-        if (!EventBus.getDefault().isRegistered(this)) {
-            EventBus.getDefault().register(this);
-        }
-
-        serviceFinishedWork = false;
-
-        // Start the service work as soon as possible!
-        SyncService.enqueueWork(this, MainApplication.getCurrentPage());
-    }
-
-    @Override
-    protected void onDestroy() {
-
-        // Be sure to unregister once this activity goes away completely
-        // NOTE: we still want to receive events if the activity is paused or stopped
-        EventBus.getDefault().unregister(this);
-
-        super.onDestroy();
+        // TODO go to main activity
     }
 }
